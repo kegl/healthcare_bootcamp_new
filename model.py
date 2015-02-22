095caf8e6d0b03b06bc60ba83207dcb925fb6503
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-22 18:52:57 +0100

    3

--- model.py
+++ model.py
@@ -4,14 +4,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
 
 class Classifier(BaseEstimator):
-	def __init__(self):
-		self.clf = Pipeline([('imputer', Imputer()), ('rf', RandomForestClassifier(n_estimators=50))])
-	
-	def fit(self, X, y):
-		self.clf.fit(X, y)
+    def __init__(self):
+        self.clf = Pipeline([('imputer', Imputer()), ('rf', RandomForestClassifier(n_estimators=50))])
 
-	def predict(self, X):
-		return self.clf.predict(X)
+    def fit(self, X, y):
+        self.clf.fit(X, y)
 
-	def predict_proba(self, X):
-		return self.clf.predict_proba(X5
+    def predict(self, X):
+        return self.clf.predict(X)
+
+    def predict_proba(self, X):
+        return self.clf.predict_proba(X)
