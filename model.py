54f197a4bde26f1ddc6666a5c9e8cc7e190db165
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-22 23:30:19 +0100

    666666

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
@@ -8,7 +8,7 @@ class Classifier(BaseEstimator):
         self.clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
         ('rf', AdaBoostClassifier(base_estimator=RandomForestClassifier(max_depth=5, n_estimators=100),
                          n_estimators=20))])
-        
+
     def fit(self, X, y):
         self.clf.fit(X, y)
 
