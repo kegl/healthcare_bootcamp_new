8c294962e725283f12a8039e687fb5dc514f0a0e
Balazs Kegl <balazs.kegl@gmail.com>
2015-03-24 18:35:19 +0100

    NuSVC

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import Imputer, StandardScaler
-from sklearn.svm import NuSVC
+from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
 
@@ -9,24 +8,5 @@ class Classifier(BaseEstimator):
         self.clf = Pipeline([('imputer', Imputer()), 
             ('rf', RandomForestClassifier(n_estimators=100))])
     
-        self.clf2 = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('svc', NuSVC(probability=True))])
-
-        self.scaler = StandardScaler()
-
-    def fit(self, X, y):
-        self.clf.fit(X, y)
-        X_good_features = self.clf.transform(X)
-        X_scaled = self.scaler.fit_transform(X_good_features)
-        self.clf2.fit(X_scaled, y)
-
-    def predict(self, X):
-        X_good_features = self.clf.transform(X)
-        X_scaled = self.scaler.fit_transform(X_good_features)
-        return self.clf2.predict(X_scaled)
-
-    def predict_proba(self, X):
-        X_good_features = self.clf.transform(X)
-        X_scaled = self.scaler.fit_transform(X_good_features)
-        return self.clf2.predict_proba(X_scaled)
-
+    def __getattr__(self, attrname):
+        return getattr(self.clf, attrname)
